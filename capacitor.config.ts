3aa7f37b26587ac9e91f2975c1680644ecc5ddba
Paula0908 <paulisms@gmail.com>
2025-09-03 17:54:11 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practico1_M2',
+  webDir: 'www'
+};
+
+export default config;
